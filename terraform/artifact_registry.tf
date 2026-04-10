61284f54300e7fea757594f057fd42add5233d25
Fajar Fadli <fajarmaftuhfadli@gmail.com>
2026-04-10 18:09:01 +0700

    Add permission to service account to get images registry and storage bucket

--- terraform/artifact_registry.tf
+++ terraform/artifact_registry.tf
@@ -3,3 +3,9 @@ resource "google_artifact_registry_repository" "nexus-repo" {
   repository_id = var.project_id
   format        = "DOCKER"
 }
+
+resource "google_artifact_registry_repository_iam_member" "gke_pull_permission" {
+  repository = google_artifact_registry_repository.nexus-repo.name
+  role       = "roles/artifactregistry.reader"
+  member     = "serviceAccount:${google_service_account.nexus_sa.email}"
+}
--- terraform/service_account.tf
+++ terraform/service_account.tf
@@ -2,3 +2,9 @@ resource "google_service_account" "nexus_sa" {
   account_id   = "nexus-sa"
   display_name = "Nexus Service Account"
 }
+
+resource "google_project_iam_member" "project" {
+  project = var.project_id
+  role    = "roles/storage.bucketViewer"
+  member  = "serviceAccount:${google_service_account.nexus_sa.email}"
+}
